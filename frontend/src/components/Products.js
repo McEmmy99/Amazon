296e01dfeeaf338c16c0346eaf8ac6a28a131cc8
Emmanuel ALabi <alabiemmanuel016@gmail.com>
2020-12-19 07:20:51 +0100

    Reuploading Frontend and Backend

---
+++ frontend/src/components/Products.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import Rating from './Rating';
+
+export default function Products(props) {
+    const product = props.product;
+    return (
+        <div>
+            <div key={product._id} className="card">
+                        <Link to={`/product/${product._id}`}>
+                            <img className="medium" src={product.image} alt={product.name}/>
+                        </Link>
+                        <div className="card-body">
+                            <Link to={`/product/${product._id}`}>
+                                <h2>{product.name}</h2>
+                            </Link>
+                            <div className="rating">
+                                <Rating rating={product.rating} numReviews={product.numReviews}/>
+                            </div>
+                            <div className="price">
+                                ${product.price}
+                            </div>
+                        </div>
+                    </div>
+        </div>
+    )
+}
